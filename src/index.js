014dde8db35cdf1f14a6c67730ed13624235faf4
Quin Hill <quinhill@icloud.com>
2018-07-10 06:16:24 -0600

    Set up redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+const store = createStore(rootReducer, devTools);
+
+ReactDOM.render(<Provider store={store}>
+  <App />
+</Provider>,
+  document.getElementById('application'), document.getElementById('root'));
 registerServiceWorker();
