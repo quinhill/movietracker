46a9d63483b4aa20b6675cb7694817d6a9b42027
Arram Mandel <airum82@gmail.com>
2018-07-12 09:55:53 -0600

    Imported redux and react testing dependencies and added App snapshot test

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,5 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
+import { Provider } from 'react-redux';
 
+describe('App', () => {
+  let store;
+  let wrapper;
+  let initialState;
+  let mockStore;
+  beforeEach(() => {
+    initialState = {
+      nowPlaying: [{}]
+    }
+    mockStore = configureStore();
+    store = mockStore(initialState)
+    wrapper = shallow(<Provider store={store}>
+                        <App />
+                      </Provider>, { disableLifeCycleMethods: true });
+  })
+
+  it('should match snapshot upon render', () => {
+
+    expect(wrapper).toMatchSnapshot();
+  })
+})
 
