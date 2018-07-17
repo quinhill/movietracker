188dff105740f22cec960b985af3f4b4be7771e6
Arram Mandel <airum82@gmail.com>
2018-07-17 11:25:19 -0600

    Added several component tests

--- src/containers/Movie.test.js
+++ src/containers/Movie.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Movie from './Movie';
-import { shallow } from 'enzyme';
+import { Movie } from './Movie';
+import { shallow, mount } from 'enzyme';
 
 describe('Movie', () => {
 
@@ -9,4 +9,42 @@ describe('Movie', () => {
 
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should call handleFavorite with correct param on click', () => {
+    const mockMovie = {
+      id: '5',
+      title: 'A quintillion sorrows',
+      overview: 'I cannot summarize the intensity in words'
+    }
+    const mockHandleFavorite = jest.fn();
+    const wrapper = mount(<Movie 
+      handleFavorite={mockHandleFavorite}
+      {...mockMovie}/>);
+    wrapper.find('button').simulate('click');
+    expect(mockHandleFavorite).toHaveBeenCalledWith(mockMovie.id);
+  })
+
+  it('button should have favorite text if movie is not favorited', () => {
+    const mockMovie = {
+      id: '5',
+      title: 'A quintillion sorrows',
+      overview: 'I cannot summarize the intensity in words',
+      favorite: false
+    };
+    const wrapper = shallow(<Movie {...mockMovie} />);
+    expect(wrapper.find('button').text()).toEqual('favorite');
+  })
+
+  it('button should have unfavorite text if movie is favorited', () => {
+    const mockMovie = {
+      id: '5',
+      title: 'A quintillion sorrows',
+      overview: 'I cannot summarize the intensity in words',
+      favorite: true
+    };
+    const wrapper = shallow(<Movie {...mockMovie} />);
+    expect(wrapper.find('button').text()).toEqual('unfavorite');
+  })
+
+
 });
--- src/containers/MovieList.test.js
+++ src/containers/MovieList.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
-import { MovieList, mapStateToProps } from './MovieList';
+import { MovieList, mapStateToProps, mapDispatchToProps } from './MovieList';
 import { shallow } from 'enzyme';
+import { toggleFavorite,
+ addUserFavorite, 
+ removeUserFavorite,
+ promptCreate } from '../actions';
 
 describe('MovieList', () => {
   let wrapper;
@@ -33,4 +37,42 @@ describe('MovieList', () => {
       expect(mappedProps).toEqual(expected);
     });
   });
+
+  describe('mapDispatchToProps', () => {
+    let mockDispatch;
+    let mappedProps;
+    beforeEach(() => {
+      mockDispatch = jest.fn();
+      mappedProps = mapDispatchToProps(mockDispatch);
+    })
+    it('should call dispatch with correct params when handle toggle is called', () => {
+      const movieId = 564;
+      const actionToDispatch = toggleFavorite(movieId)
+      mappedProps.handleToggle(movieId);
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    })
+
+    it('should call dispatch with correct params when handleAddFav is called', () => {
+      const mockFavoriteMovie = { 
+        title: 'favorites',
+        overview: 'everyone has them'
+      }
+      const actionToDispatch = addUserFavorite(mockFavoriteMovie);
+      mappedProps.handleAddFav(mockFavoriteMovie);
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    })
+
+    it('should call dispatch with correct params when removeUserFavs is called', () => {
+      const mockMovieId = 453;
+      const actionToDispatch = removeUserFavorite(mockMovieId);
+      mappedProps.handleRemoveFav(mockMovieId);
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    })
+
+    it('should call dispatch with correct params when promptCreate is called', () => {
+      const actionToDispatch = promptCreate();
+      mappedProps.promptCreate();
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    })
+  })
 });
